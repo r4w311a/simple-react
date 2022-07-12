7d3750694e38ec106f4879e864222a0d4e8b36dd
Hussein Malkawi <r4w311a@gmail.com>
2022-07-12 21:39:40 +0300

    create date component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem name={Expenses[0].name} price={Expenses[0].price} date={Expenses[0].date}></ExpenseItem>
-      <ExpenseItem name={Expenses[1].name} price={Expenses[1].price} date={Expenses[1].date}></ExpenseItem>
-      <ExpenseItem name={Expenses[2].name} price={Expenses[2].price} date={Expenses[2].date}></ExpenseItem>
-      <ExpenseItem name={Expenses[3].name} price={Expenses[3].price} date={Expenses[3].date}></ExpenseItem>
+      <ExpenseItem name={Expenses[0].name} price={Expenses[0].price} date={Expenses[0].date} />
+      <ExpenseItem name={Expenses[1].name} price={Expenses[1].price} date={Expenses[1].date} />
+      <ExpenseItem name={Expenses[2].name} price={Expenses[2].price} date={Expenses[2].date} />
+      <ExpenseItem name={Expenses[3].name} price={Expenses[3].price} date={Expenses[3].date} />
     </div>
   );
 }
